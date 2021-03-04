1f62510af1fa17a6b1fed184370b24860dee7df3
Luis Gustavo <gustascrows@gmail.com>
2021-03-04 17:26:22 -0300

    Feat: Criação da tela de edicão de listagem

---
+++ src/pages/EditListScreen/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {
+    Container, Text
+} from './styles';
+
+export default () =>{
+    return(
+        <Container>
+            <Text>Edit Screen</Text>
+        </Container>
+    );
+}
