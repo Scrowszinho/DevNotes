be78808211aa84c8e5b0ab5e8cb581986a9127b0
Luis Gustavo <gustascrows@gmail.com>
2021-03-08 17:46:08 -0300

    Screen: Dando inicio a tela de edit note

--- src/pages/EditListScreen/index.js
+++ src/pages/EditListScreen/index.js
@@ -1,12 +1,29 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {
-    Container, Text
+    Container, Text,
+    TitleInput, BodyInput
 } from './styles';
 
 export default () =>{
+    const [title, setTitle] = useState('');
+    const [body, setBody] = useState('');
+
     return(
         <Container>
-            <Text>Edit Screen</Text>
+            <TitleInput placeholder="Note title" 
+            placeholderTextColor='#CCC'
+            value = {title} 
+            onChangeText={t=>setTitle(t)}
+            autoFocus={true}
+            />
+            <BodyInput 
+            placeholder="Note text" 
+            placeholderTextColor='#CCC'
+            multiline={true}
+            textAlignVertical='top'
+            value = {body} 
+            onChangeText={t=>setBody(t)}
+            />
         </Container>
     );
 }
--- src/pages/EditListScreen/styles.js
+++ src/pages/EditListScreen/styles.js
@@ -1,5 +1,22 @@
 import styled from 'styled-components/native';
 
-export const Container = styled.SafeAreaView``;
+export const Container = styled.SafeAreaView`
+flex:1;
+background-color:#333;
+`;
 
 export const Text = styled.Text``;
+
+export const  TitleInput = styled.TextInput`
+font-size:20px;
+font-weight:bold;
+padding:15px;
+color:#FFF;
+`;
+
+export const  BodyInput = styled.TextInput`
+flex:1;
+padding:15px;
+font-size:15px;
+color: #FFF;
+`;
