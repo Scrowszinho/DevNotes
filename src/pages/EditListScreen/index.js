5d66accd2b37d0d2583183513e182b141fc50fc8
Luis Gustavo <gustascrows@gmail.com>
2021-03-08 18:07:29 -0300

    Feat: Lendo o index da nota a ser alterada

--- src/pages/EditListScreen/index.js
+++ src/pages/EditListScreen/index.js
@@ -1,12 +1,30 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect, useLayoutEffect} from 'react';
+import {useSelector, useDispatch} from 'react-redux';
+import {useNavigation, useRoute} from '@react-navigation/native';
+
 import {
     Container, Text,
     TitleInput, BodyInput
 } from './styles';
 
 export default () =>{
+
+    const navigation = useNavigation();
+    const route = useRoute();
+    const dispatch = useDispatch();
+    const list = useSelector(state => state.notes.list);
+
     const [title, setTitle] = useState('');
     const [body, setBody] = useState('');
+    const [status, setStatuts] = useState('new');
+
+    useEffect(()=>{
+        if(route.params?.key != undefined && list[route.params.key]){
+            setStatuts('edit');
+            setTitle(list[route.params.key].title);
+            setBody(list[route.params.key].body);
+        }    
+    },[]);
 
     return(
         <Container>
