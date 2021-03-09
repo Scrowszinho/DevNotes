b291603aaff6350368edab573fe70db558821cfe
Luis Gustavo <gustascrows@gmail.com>
2021-03-09 17:53:26 -0300

    Feat: Layouteffect adicionado

--- src/pages/EditListScreen/index.js
+++ src/pages/EditListScreen/index.js
@@ -1,13 +1,15 @@
-import React, {useState, useEffect, useLayoutEffect} from 'react';
-import {useSelector, useDispatch} from 'react-redux';
-import {useNavigation, useRoute} from '@react-navigation/native';
+import React, { useState, useEffect, useLayoutEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { useNavigation, useRoute } from '@react-navigation/native';
 
 import {
     Container, Text,
-    TitleInput, BodyInput
+    TitleInput, BodyInput,
+    SaveButton, SaveButtonImage,
+    CloseButton, CloseButtonImage
 } from './styles';
 
-export default () =>{
+export default () => {
 
     const navigation = useNavigation();
     const route = useRoute();
@@ -18,29 +20,59 @@ export default () =>{
     const [body, setBody] = useState('');
     const [status, setStatuts] = useState('new');
 
-    useEffect(()=>{
-        if(route.params?.key != undefined && list[route.params.key]){
+    const handleSaveButton = () => {
+        if (title == '' || body == '') {
+        alert('Title and description is empty!')
+        } else {
+            
+        }
+    }
+
+    const handleCloseButton = () => {
+        navigation.goBack();
+    }
+
+    useEffect(() => {
+        if (route.params?.key != undefined && list[route.params.key]) {
             setStatuts('edit');
             setTitle(list[route.params.key].title);
             setBody(list[route.params.key].body);
-        }    
-    },[]);
+        }
+    }, []);
 
-    return(
+
+    useLayoutEffect(() => {
+        navigation.setOptions({
+            title: status == 'new' ? 'New Note' : 'Edit Note',
+            headerLeft: () => (
+                <CloseButton underlayColor='transparent' onPress={handleCloseButton}>
+                    <CloseButtonImage source={require('../../assets/close.png')} />
+                </CloseButton>
+            ),
+            headerRight: () => (
+                <SaveButton underlayColor='transparent' onPress={handleSaveButton}>
+                    <SaveButtonImage source={require('../../assets/save.png')} />
+                </SaveButton>
+            )
+        })
+    }, [status, title, body]);
+
+
+    return (
         <Container>
-            <TitleInput placeholder="Note title" 
-            placeholderTextColor='#CCC'
-            value = {title} 
-            onChangeText={t=>setTitle(t)}
-            autoFocus={true}
+            <TitleInput placeholder="Note title"
+                placeholderTextColor='#CCC'
+                value={title}
+                onChangeText={t => setTitle(t)}
+                autoFocus={true}
             />
-            <BodyInput 
-            placeholder="Note text" 
-            placeholderTextColor='#CCC'
-            multiline={true}
-            textAlignVertical='top'
-            value = {body} 
-            onChangeText={t=>setBody(t)}
+            <BodyInput
+                placeholder="Note text"
+                placeholderTextColor='#CCC'
+                multiline={true}
+                textAlignVertical='top'
+                value={body}
+                onChangeText={t => setBody(t)}
             />
         </Container>
     );
